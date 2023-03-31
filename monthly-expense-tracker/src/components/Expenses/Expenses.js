8f5767daa6e30be775ec75b621986db7d7eb35e9
Shoaib Ahmed <96101435+shoaib-ahmed16@users.noreply.github.com>
2023-03-31 16:38:34 +0530

    Add the files

--- monthly-expense-tracker/src/components/Expenses/Expenses.js
+++ monthly-expense-tracker/src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import './Expenses.css'
 import Card from '../UI/Card'
 import ExpensesFilter from '../NewExpense/ExpressFilter'
 import ExpensesList from './ExpensesList'
+import ExpensesChart from './ExpensesChart'
 const Expenses = (props) => {
   const [filteredYear,setFilteredYear]=useState('2019')
   const filterChangeHandler =(selectedyear) =>{
@@ -26,6 +27,7 @@ const Expenses = (props) => {
     <div>
       <Card className='expenses'>
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+      <ExpensesChart expenses={filterExpenses}/>
       {/* {expenseContent} */}
       <ExpensesList data ={filterExpenses}/>
       {/* <ExpenseItem
