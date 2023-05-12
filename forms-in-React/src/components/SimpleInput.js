81c086fd227c996e670abe8ff079302cd7ffb1f2
Shoaib Ahmed <96101435+shoaib-ahmed16@users.noreply.github.com>
2023-05-12 21:42:56 +0530

    Refactoring and reducing the code by removing use of useState hooks

--- forms-in-React/src/components/SimpleInput.js
+++ forms-in-React/src/components/SimpleInput.js
@@ -1,63 +1,40 @@
-import React,{useRef,useState,useEffect} from "react";
+import React,{useState,useEffect} from "react";
 const SimpleInput = (props) => {
-
-  const nameInputRef =useRef();
 
   const [enteredName,setEnterName] =useState("");
 
-  const [enteredNameIsValid,setEnterNameIsValid] =useState(false);
-
   const [enteredNameTouched,setEnterNameTouched] =useState(false);
 
-  const nameInputBlurHandler =event =>{
-    setEnterNameTouched(true);
-     if(enteredName.trim()===""){
-      setEnterNameIsValid(false);
-      return;
-    }
-  }
+  const enteredNameIsValid = enteredName.trim() !=='';
+  const nameInputIsValid =!enteredNameIsValid && enteredNameTouched;
 
   const nameInputChangehandler =(event)=>{
   setEnterName(event.target.value);
   }
 
-  useEffect(()=>{
-    if(enteredNameIsValid){
-
-      console.log("Name Input is Valid!");
-
-    }
-  },[enteredNameIsValid]);
-
+ const nameInputBlurHandler =event =>{
+    setEnterNameTouched(true);
+  }
   const formSubmissionHandler =event =>{
-
     event.preventDefault();
 
     setEnterNameTouched(true);
 
-    if(enteredName.trim()===""){
-      setEnterNameIsValid(false);
-      return;
+    if(!enteredNameIsValid){
+      return
     }
-    setEnterNameIsValid(true);
 
-    console.log(enteredName);
-
-    const enterValue =nameInputRef.current.value
-
-    console.log("useRef",enterValue);
    // nameInputRef.current.value ="" this directly manupulating the dom this is not according to the concept of React which work on virtul dom manupulation.
     setEnterName('')
+    setEnterNameTouched(false);
   }
-
-  const nameInputIsValid =!enteredNameIsValid && enteredNameTouched;
 
   const nameInputClass =nameInputIsValid ? 'form-control invalid':'form-control';
   return (
     <form>
       <div className={nameInputClass}>
         <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangehandler} value={enteredName} onBlur={nameInputBlurHandler} />
+        <input type='text' id='name' onChange={nameInputChangehandler} value={enteredName} onBlur={nameInputBlurHandler} />
         {nameInputIsValid && <p className="error-text">Name Must Not Be Empty.</p>}
       </div>
       <div className="form-actions">
